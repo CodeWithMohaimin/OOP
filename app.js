cd4cca81c4ec6b9d116ad9165083656cf9082b41
programmermohaimin <you@example.com>
2022-09-28 17:39:51 +0600

    adding some array method

--- app.js
+++ app.js
@@ -235,5 +235,30 @@ console.log(' my string is '+str); //Nothing change in string
 
 
 
-// ================Function is a Object===============//
-//   object oriented programming is awesome,
+const arr = [
+    { id: 1, value: 10 },
+    { id: 2, value: 20 },
+    { id: 3, value: 30 },
+    { id: 4, value: 40 },
+    { id: 5, value: 50 },
+];
+
+
+const index2 = arr.find((item) => item.id === 4)
+console.log(index2);
+
+
+
+// Filter --> immutable
+
+const arr1 = arr.filter((item) => item.id !== 4 )
+console.log(arr1);
+
+
+// Filter --> mutable
+
+const index = arr.findIndex((item) => item.id === 4)
+console.log(index);
+
+const arr2 = arr.splice(index, 1)
+console.log(arr);
---
+++ index.js
@@ -0,0 +1,21 @@
+const arr = [
+    { name: "mohaimin", price: "20$" },
+    { name: "ga", price: "20$" },
+    { name: "grr", price: "20$" },
+    { name: "vgr", price: "20$" },
+    { name: "eve", price: "20$" },
+    { name: "bhr", price: "20$" },
+    { name: "li", price: "20$" },
+    { name: "feg", price: "20$" },
+    { name: "hrc", price: "20$" },
+    { name: "66b", price: "20$" },
+    { name: "jyj", price: "20$" },
+    { name: "ty6", price: "20$" },
+    { name: "tyy", price: "20$" },
+    
+]
+
+
+let getName = arr.filter((nam) => {
+    console.log(nam.name, nam.price);
+});
